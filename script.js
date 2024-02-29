de81c4eabfc7c88bd00686e2bed32dbad7276997
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-02-29 14:57:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function mincost(arr)
 { 
-//write your code here
+//SGN write your code here
+	
+  arr.sort(function(a,b){ return a-b });
+  let sum =0;
+  let res = 0;
+  
+  let sumArr = [0];
+  for(let i=0; i<arr.length; i++){
+      sum += arr[i];
+      sumArr.push(sum);
+        
+}
+for(let j=0; j<sumArr.length; j++){
+    res += sumArr[j];
+}
+
+	return (res-sumArr[1]);
+
 // return the min cost
   
 }
